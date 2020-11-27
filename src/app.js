2c7af13f074f89e89fd19d4ec26458c775f57d12
Sireini <smgj.reinieren@gmail.com>
2020-11-27 14:41:59 +0100

    add nodemon

--- src/app.js
+++ src/app.js
@@ -16,45 +16,42 @@ const MollieController = require('./controllers/Mollie.controller');
 
 const app = express();
 
-// TO DO DELETE
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors());
-// ------------ 
-
 
 app.use(express.json())
 app.use(demoRouter)
 
 app.use("/*", (req, res, next) => {
     res.error = (err) => {
-      return res.status(500).json({
-        status: 500,
-        data: null,
-        message: err,
-        accessToken: "",
-      });
+        return res.status(500).json({
+            status: 500,
+            data: null,
+            message: err,
+            accessToken: "",
+        });
     };
-  
+
     res.success = (data, accessToken = "", msg = "") => {
-      return res.status(200).json({
-        status: 200,
-        data: data,
-        accessToken: accessToken,
-        message: msg,
-      });
+        return res.status(200).json({
+            status: 200,
+            data: data,
+            accessToken: accessToken,
+            message: msg,
+        });
     };
-  
+
     res.unauthorized = () => {
-      return res.status(401).send({
-        auth: false,
-        message:
-          "Je sessie is verlopen en bent automatisch uitgelogd. Log opnieuw in.",
-        status: 401,
-        data: null,
-        accessToken: accessToken,
-      });
+        return res.status(401).send({
+            auth: false,
+            message:
+                "Je sessie is verlopen en bent automatisch uitgelogd. Log opnieuw in.",
+            status: 401,
+            data: null,
+            accessToken: accessToken,
+        });
     };
-  
+
     next();
 });
 
