933c3bdd854c425ce5386f8a150951a855cbb8a5
Sireini <smgj.reinieren@gmail.com>
2020-12-10 17:26:43 +0100

    Update Mollie.controller.js

--- src/controllers/Mollie.controller.js
+++ src/controllers/Mollie.controller.js
@@ -8,29 +8,104 @@ module.exports = function (
     const User = require('../models/User.model');
     const MollieSubscription = require('../models/Mollie-Subscription.model');
 
-  /*
-   **
-   ** Financi Standard - Mollie €1 subscription
-   ** 1. Create customer
-   ** 2. Create first payment
-   ** 3. Create Subscription
-   **
-   **/
+    /*
+     **
+     ** Financi Standard - Mollie €1 subscription
+     ** 1. Create customer
+     ** 2. Create first payment
+     ** 3. Create Subscription
+     **
+     **/
     app.post(
-    "/api/mollie/investly-standard/create-customer",
-    VerifyToken,
-    async (req, res) => {
-        if (req.body != null && req.body != undefined) {
-            let userId = req.userId;
-            let user = await User.findById(userId).lean().exec();
+        "/api/mollie/investly-standard/create-customer",
+        VerifyToken,
+        async (req, res) => {
+            if (req.body != null && req.body != undefined) {
+                let userId = req.userId;
+                let user = await User.findById(userId).lean().exec();
 
-            if (user) {
-                let requestData = { name: `${user.firstname} ${user.lastname}`, email: user.email };
+                if (user) {
+                    let requestData = { name: `${user.firstname} ${user.lastname}`, email: user.email };
+                    let request = require("request");
+
+                    let options = {
+                        method: "POST",
+                        url: "https://api.mollie.com/v2/customers",
+                        headers: {
+                            Authorization: "Bearer " + apiKey,
+                        },
+                        form: requestData,
+                    };
+
+                    request(options, async (error, response, body) => {
+                        if (error) {
+                            return res.status(422).json({
+                                status: 422,
+                                data: error,
+                                message: "Failed to create customer",
+                                token: "",
+                            });
+                        }
+
+                        let data = JSON.parse(body);
+
+                        let updateData = { $set: { mollie_customerId: data.id } };
+                        let updateUser = await User.findOneAndUpdate(
+                            { _id: userId },
+                            updateData
+                        );
+
+                        if (updateUser && Object.keys(updateUser).length > 0) {
+                            res.success(JSON.parse(body));
+                        } else {
+                            res.error("Failed to update customerId in user collection");
+                        }
+                    });
+                } else {
+                    res.error("No user record found for the given userId");
+                }
+            }
+        });
+
+    /**
+    * Create first payment
+    */
+    app.post(
+        "/api/user/:customerId/mollie/investly-standard/create-payment",
+        async (req, res) => {
+            if (req.body) {
+                let customerId = req.params.customerId;
+                let paymentId = mongoose.mongo.ObjectId();
+                let subscriptionPlan = req.body.subscriptionPlan;
+
+                console.log(req.body)
+
+                let webhookUrl =
+                    "https://investly.nl/api/investly-standard/" + subscriptionPlan + "/payment/" + req.body.UserId;
+
+                // let redirectUrl = "https://investly.nl/login?customerId=" + customerId;
+                let redirectUrl = "https://investly.nl/dashboard/";
+
+                if (!customerId) {
+                    return res.error("Please send a valid Mollie Customer Id");
+                }
+
+                let requestData = {
+                    "amount[currency]": "EUR",
+                    "amount[value]": "1.21",
+                    customerId: customerId,
+                    sequenceType: "first",
+                    description: "Investly Standard First Payment - " + customerId,
+                    metadata: { UserId: req.body.UserId, Type: "Standard" },
+                    redirectUrl: redirectUrl,
+                    webhookUrl: webhookUrl,
+                };
+
                 let request = require("request");
 
                 let options = {
                     method: "POST",
-                    url: "https://api.mollie.com/v2/customers",
+                    url: "https://api.mollie.com/v2/payments",
                     headers: {
                         Authorization: "Bearer " + apiKey,
                     },
@@ -42,121 +117,63 @@ module.exports = function (
                         return res.status(422).json({
                             status: 422,
                             data: error,
-                            message: "Failed to create customer",
+                            message: "Failed to create a payment",
                             token: "",
                         });
                     }
 
                     let data = JSON.parse(body);
 
-                    let updateData = { $set: { mollie_customerId: data.id } };
-                    let updateUser = await User.findOneAndUpdate(
-                        { _id: userId },
-                        updateData
-                    );
+                    let mijnMenuSubscription = new MollieSubscription();
+                    mijnMenuSubscription._id = paymentId;
+                    mijnMenuSubscription.status = data.status;
+                    mijnMenuSubscription.paymentId = data.id;
+                    mijnMenuSubscription.customerId = customerId;
+                    mijnMenuSubscription.profileId = data.profileId;
 
-                    if (updateUser && Object.keys(updateUser).length > 0) {
-                        res.success(JSON.parse(body));
-                    } else {
-                        res.error("Failed to update customerId in user collection");
-                    }
+                    mijnMenuSubscription.type = "Standard";
+                    mijnMenuSubscription.resource = data.resource;
+                    mijnMenuSubscription.sequenceType = data.sequenceType;
+                    mijnMenuSubscription.metadata = data.metadata;
+                    mijnMenuSubscription.method = data.method;
+                    mijnMenuSubscription.description = data.description;
+                    mijnMenuSubscription.amount = data.amount;
+                    mijnMenuSubscription.createdAt = data.createdAt;
+                    mijnMenuSubscription.recordCreatedAt = new Date();
+
+                    await mijnMenuSubscription.save();
+                    res.success(data);
                 });
-            } else {
-                res.error("No user record found for the given userId");
             }
-        }
-    });
-
-   /**
-   * Create first payment
-   */
-    app.post(
-        "/api/user/:customerId/mollie/investly-standard/create-payment",
-        async (req, res) => {
-        if (req.body) {
-            let customerId = req.params.customerId;
-            let paymentId = mongoose.mongo.ObjectId();
-
-            let webhookUrl =
-                "https://investly.nl/api/investly-standard/payment/" + req.body.UserId;
-
-            let redirectUrl = "https://investly.nl/login?customerId=" + customerId;
-
-            if (!customerId) {
-                return res.error("Please send a valid Mollie Customer Id");
-            }
-
-            let requestData = {
-                "amount[currency]": "EUR",
-                "amount[value]": "1.21",
-                customerId: customerId,
-                sequenceType: "first",
-                description: "Investly Standard First Payment - " + customerId,
-                metadata: { UserId: req.body.UserId, Type: "Standard" },
-                redirectUrl: redirectUrl,
-                webhookUrl: webhookUrl,
-            };
-
-            let request = require("request");
-
-            let options = {
-                method: "POST",
-                url: "https://api.mollie.com/v2/payments",
-                headers: {
-                    Authorization: "Bearer " + apiKey,
-                },
-                form: requestData,
-            };
-
-            request(options, async (error, response, body) => {
-                if (error) {
-                    return res.status(422).json({
-                        status: 422,
-                        data: error,
-                        message: "Failed to create a payment",
-                        token: "",
-                    });
-                }
-
-                let data = JSON.parse(body);
-
-                let mijnMenuSubscription = new MollieSubscription();
-                mijnMenuSubscription._id = paymentId;
-                mijnMenuSubscription.status = data.status;
-                mijnMenuSubscription.paymentId = data.id;
-                mijnMenuSubscription.customerId = customerId;
-                mijnMenuSubscription.profileId = data.profileId;
-
-                mijnMenuSubscription.type = "Standard";
-                mijnMenuSubscription.resource = data.resource;
-                mijnMenuSubscription.sequenceType = data.sequenceType;
-                mijnMenuSubscription.metadata = data.metadata;
-                mijnMenuSubscription.method = data.method;
-                mijnMenuSubscription.description = data.description;
-                mijnMenuSubscription.amount = data.amount;
-                mijnMenuSubscription.createdAt = data.createdAt;
-                mijnMenuSubscription.recordCreatedAt = new Date();
-
-                await mijnMenuSubscription.save();
-                res.success(data);
-            });
-        }
-    });
+        });
 
     /**
      * Handle webhook url for first payment
      * Called by Mollie webhook to update status of subscription
      */
-    app.post("/api/investly-standard/payment/:userId", async (req, res) => {
+    app.post("/api/investly-standard/:subscriptionPlan/payment/:userId", async (req, res) => {
         try {
-            console.log("WEBHOOK DATA FOR PAYMENT!", req.body);
             let paymentId = req.body.id;
             let userId = req.params.userId;
             let paymentRequest = await getSubscriptionPayment(
                 paymentId
             );
+            let subscriptionPlan = req.params.subscriptionPlan;
+            let subscriptionInterval;
+            let subscriptionAmount;
+
             let webhookUrl =
                 "https://investly.nl/api/investly/subscription/webhook";
+
+            if (subscriptionPlan === "monthly") {
+                subscriptionInterval = "1 month"
+                subscriptionAmount = "5.99";
+            }
+
+            if (subscriptionPlan === "yearly") {
+                subscriptionInterval = "12 months";
+                subscriptionAmount = "65.34"
+            }
 
             if (!paymentRequest && paymentRequest.status !== "paid") {
                 return res.error("Unable to get payment request or you did not pay");
@@ -180,8 +197,8 @@ module.exports = function (
             let payment = await MijnMenuSubscriptions.find({
                 paymentId: paymentRequest.id,
             })
-            .lean()
-            .exec();
+                .lean()
+                .exec();
 
             if (!payment) {
                 return res.error("Sorry unable to find userpayment");
@@ -189,15 +206,17 @@ module.exports = function (
 
             let requestData = {
                 "amount[currency]": "EUR",
-                "amount[value]": "1.21",
-                interval: "1 month",
+                "amount[value]": subscriptionAmount,
+                interval: subscriptionInterval,
                 metadata: { UserId: userId, Type: "Standard" },
                 mandateId: mandate.id,
                 description:
-                    "Investly Standard - Monthly Subscription - " +
+                    `Investly Standard - ${subscriptionPlan} Subscription - ` +
                     paymentRequest.customerId,
                 webhookUrl: webhookUrl,
             };
+
+            console.log(requestData);
 
             // Create Subscription
             let createMollieSubscription = await createSubscription(
@@ -226,11 +245,11 @@ module.exports = function (
             mijnMenuSubscription.amount = createMollieSubscription.amount;
             mijnMenuSubscription.times = createMollieSubscription.times;
             mijnMenuSubscription.timesRemaining =
-            createMollieSubscription.timesRemaining;
+                createMollieSubscription.timesRemaining;
             mijnMenuSubscription.interval = createMollieSubscription.interval;
             mijnMenuSubscription.startDate = createMollieSubscription.startDate;
             mijnMenuSubscription.nextPaymentDate =
-            createMollieSubscription.nextPaymentDate;
+                createMollieSubscription.nextPaymentDate;
             mijnMenuSubscription.createdAt = createMollieSubscription.createdAt;
             mijnMenuSubscription.recordCreatedAt = new Date();
 
@@ -265,8 +284,8 @@ module.exports = function (
                 },
                 { new: true }
             )
-            .lean()
-            .exec();
+                .lean()
+                .exec();
 
             if (!user) {
                 return res.error("Sorry unable to find a user with this id.");
@@ -285,7 +304,7 @@ module.exports = function (
      * Handle webhook url Recurring payment
      * Called by Mollie to store a new Mijn Menu Standard or Plus payment
      */
-    app.post("/api/mijn-menu/subscription/webhook", async (req, res) => {
+    app.post("/api/investly/subscription/webhook", async (req, res) => {
         try {
             console.log("WEBHOOK DATA", req.body, JSON.parse(body));
             let subscriptionId = req.body.id;
@@ -293,16 +312,16 @@ module.exports = function (
                 subscriptionId: subscriptionId,
                 resource: "subscription",
             })
-            .lean()
-            .exec();
+                .lean()
+                .exec();
 
             if (!subscription) {
                 return res.error("Unable to find subscription in database");
             }
 
             let mollieSubscription = await getSubscription(
-            subscription.customerId,
-            subscriptionId
+                subscription.customerId,
+                subscriptionId
             );
 
             if (!mollieSubscription) {
@@ -342,9 +361,6 @@ module.exports = function (
         }
     });
 
-    const createCustomer = async () => {
-
-    }
 
     /**
      * get Subscription Payment - Mijn Menu Standard
@@ -363,7 +379,7 @@ module.exports = function (
         return await makeRequest(options);
     };
 
-  
+
     /**
      * get Mijn Menu Standard Subscription
      * @param {*} customerId
@@ -373,7 +389,7 @@ module.exports = function (
         let options = {
             method: "GET",
             url:
-            "https://api.mollie.com/v2/customers/" +
+                "https://api.mollie.com/v2/customers/" +
                 customerId +
                 "/subscriptions/" +
                 subscriptionId,
@@ -385,7 +401,7 @@ module.exports = function (
 
         return await makeRequest(options);
     };
-  
+
     /**
      * get Mandate details of customer - Mijn Menu Standard
      * @param {*} customerId
@@ -395,7 +411,7 @@ module.exports = function (
         let options = {
             method: "GET",
             url:
-            "https://api.mollie.com/v2/customers/" +
+                "https://api.mollie.com/v2/customers/" +
                 customerId +
                 "/mandates/" +
                 mandateId,
@@ -418,14 +434,14 @@ module.exports = function (
             method: "POST",
             url: "https://api.mollie.com/v2/customers/" + customerId + "/subscriptions",
             headers: {
-            Authorization: "Bearer " + apiKey,
+                Authorization: "Bearer " + apiKey,
             },
             form: requestObject,
         };
 
         return await makeRequest(options);
     };
-  
+
     /**
      * Delete Subscription Payment - Mijn Menu Standard
      * @param {*} molliePaymentId
@@ -434,12 +450,12 @@ module.exports = function (
         let options = {
             method: "DELETE",
             url:
-            "https://api.mollie.com/v2/customers/" +
-            customerId +
-            "/subscriptions/" +
-            subscriptionId,
+                "https://api.mollie.com/v2/customers/" +
+                customerId +
+                "/subscriptions/" +
+                subscriptionId,
             headers: {
-            Authorization: "Bearer " + apiKey,
+                Authorization: "Bearer " + apiKey,
             },
         };
 
