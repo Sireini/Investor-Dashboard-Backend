22af4d2d12a2e7d0ccecc3c5f0caccf523677e7f
Sireini <smgj.reinieren@gmail.com>
2020-12-19 17:35:38 +0100

    Update Portfolio.controller.js

--- src/controllers/Portfolio.controller.js
+++ src/controllers/Portfolio.controller.js
@@ -205,8 +205,8 @@ module.exports = function (
                         date = year + '-' + month + '-' + dt;
 
                         // return dayPrice;
-                        console.log(date = { dayPrice })
-                        return date = { dayPrice };
+                        // console.log(date = { dayPrice })
+                        return { date: dayPrice };
                     });
                     console.log('daily Crypto Price', dailyPrices);
 
